d8fc6353e6e8bcb1038f2387431cdb1be091649a
k <mykhailo.swe@gmail.com>
2024-04-08 21:28:57 +0200

    add paging logic to the filterByTag script

--- scripts/filterByTag.js
+++ scripts/filterByTag.js
@@ -1,17 +1,39 @@
-const httpRequest = require('./helpers/httpRequest');
+const httpRequest = require("./helpers/httpRequest");
 
-async function fetchCloudcastsByTag(tagName) {
-    const url = `http://localhost:1337/api/cloudcasts?filters[tags][name][$eq]=${encodeURIComponent(tagName)}&populate=tags`;
-    const method = 'GET';
-    try {
-        const data = await httpRequest(url, method);
-        return data;
-    } catch (error) {
-        throw new Error(`Error fetching cloudcasts by tag: ${error.message}`);
-    }
+async function fetchCloudcastsByTag(tagName, offset = 0, limit = 20) {
+  const url = `http://localhost:1337/api/cloudcasts?filters[tags][name][$eq]=${encodeURIComponent(
+    tagName
+  )}&populate=tags&pagination[page]=${offset / limit + 1
+    }&pagination[pageSize]=${limit}`;
+  const method = "GET";
+
+  try {
+    const data = await httpRequest(url, method);
+    return data;
+  } catch (error) {
+    throw new Error(`Error fetching cloudcasts by tag: ${error.message}`);
+  }
 }
 
-// Example usage:
-fetchCloudcastsByTag('Trance')
-    .then(data => console.log(data))
-    .catch(error => console.error(error.message));
+// Example Usage (Fetching all pages)
+async function fetchAllCloudcastsByTag(tagName) {
+  let offset = 0;
+  const allCloudcasts = [];
+  let hasMorePages = true; // Assume there are more pages initially
+
+  while (hasMorePages) {
+    const { data, meta } = await fetchCloudcastsByTag(tagName, offset, 20);
+    allCloudcasts.push(...data);
+    offset += 20;
+
+    // Check if there are more pages
+    hasMorePages = meta?.pagination?.total > offset;
+  }
+
+  return allCloudcasts;
+}
+
+
+fetchAllCloudcastsByTag("Techno")
+  .then((allCloudcasts) => console.log(allCloudcasts))
+  .catch((error) => console.error(error.message));
